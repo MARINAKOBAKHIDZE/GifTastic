44e5af81de43a9cd92e9d20c383dcfb0fe682ddc
Marina Kobakhidze <42823763+MARINAKOBAKHIDZE@users.noreply.github.com>
2019-01-19 18:45:18 -0800

    Add files via upload

--- Giphy/index.js
+++ Giphy/index.js
@@ -1,21 +1,82 @@
-$(document).ready(function(){
-   
-    $("#dogs").on("click", function(){
+$(document).ready(function () {
+
+    $("#dogs").on("click", function () {
+        $(".allImages").empty();
         $.ajax({
             url: "http://api.giphy.com/v1/gifs/search?q=dog&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
             method: "GET"
-        }).then(function(response){
-            //loop over response.data
-            //add an image tag
-            for (var i = 0; i<response.data; i++);
-            var gifObject = response.data[0];
-            console.log(gifObject.embed_url);
-        }) 
-    });
-});
-    
-    
-//figure out a way to change the qx
-// how to add other peramiters to url
-//rating
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
 
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+            }
+        })
+    });
+
+    $("#cats").on("click", function () {
+        $(".allImages").empty();
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=cat&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+            }
+
+        })
+    });
+
+    $("#fishes").on("click", function () {
+        $(".allImages").empty();
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=fish&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+                //     var gifObject = response.date[0];
+                // console.log(gifObject.embed_url);
+            }
+        })
+    });
+    $("#snakes").on("click", function () {
+        $(".allImages").empty();
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=snake&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+            }
+        })
+    });
+    $("#horses").on("click", function () {
+        $(".allImages").empty();
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=horse&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+            }
+        })
+    });
+    $("#monkeys").on("click", function () {
+        $(".allImages").empty();
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=monkey&api_key=RxZtIt2Ede18zGcy4gBa5nSxsusUM3LB&limit=5",
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.data.length; i++) {
+                $(".allImages").append("<img src=" + response.data[i].images.fixed_height.url + ">")
+            }
+        })
+    });
+
+});
